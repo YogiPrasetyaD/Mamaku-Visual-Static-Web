96551f3cac92cf02721b7e80bd219cd2ae492013
YogiPrasetyaD <yogidharmawan6@gmail.com>
2025-08-07 12:31:22 +0800

    Frontend Base: Add components and update styles

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=''>
+      <Image
+        src="/logo.png"
+        alt="Logo"
+        width={500}
+        height={500}
+        className="cursor-pointer"
+      />
+    </div>
+  )
+}
+
+export default Navbar
