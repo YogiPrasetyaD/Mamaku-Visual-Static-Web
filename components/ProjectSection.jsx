05d8c9952307b4536af37ee5f7138770490dd8a6
YogiPrasetyaD <yogidharmawan6@gmail.com>
2025-09-03 15:31:23 +0800

    perbaiki bug reload

--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -101,14 +101,13 @@ const ProjectSection = () => {
     // }, [isExpanded]);
 
     useEffect(() => {
-        if (isExpanded !== null && cardRefs.current[isExpanded]) {
-            const element = cardRefs.current[isExpanded];
-            const yOffset = -80; // offset biar ga ketutupan header
-            const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-
-            window.scrollTo({ top: y, behavior: "smooth" });
+    if (isExpanded !== null && cardRefs.current[isExpanded]) {
+        const timeout = setTimeout(() => {
+            cardRefs.current[isExpanded]?.scrollIntoView({ behavior: 'smooth', block: 'start' })
+        }, 550) // sesuai durasi animation
+        return () => clearTimeout(timeout)
         }
-    }, [isExpanded]);
+    }, [isExpanded])
 
     useEffect(() => {
         const checkMobile = () => { setisMobile(window.innerWidth < 768) };
